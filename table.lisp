15f8eec47166a08657148abc1fdc9d3a40441c76
Bibek Panthi <bpanthi977@gmail.com>
2020-07-15 22:02:09 +0545

    before changing to N=16 from N=40/minHeight = 13

--- table.lisp
+++ table.lisp
@@ -1,157 +1,8 @@
-(push (truename (make-pathname :name  "../moi/" :type nil :defaults *load-pathname*)) asdf:*central-registry*)
-(ql:quickload :moi)
+
 (in-package :moi)
 (ql:quickload :parse-float)
 (ql:quickload :cl-csv)
-(defparameter *table* nil)
-(defparameter *table-float-digits* 3)
-(defclass table ()
-  ((list :initform nil :type list)
-   (posi :initform 0 :type integer)
-   (posj :initform 0 :type integer)))
 
-(defun table-handle-floats (list)
-  (cond ((listp list)
-	 (mapcar #'table-handle-floats list))
-	((typep list 'float)
-	 (if *table-float-digits*
-	     (format nil "~,vf" *table-float-digits* list)
-	     list))
-	((typep list 'number)
-	 (if *table-float-digits*
-	     (format nil "~,vf" *table-float-digits* (coerce list 'float))
-	     list))
-	(t list)))
-
-(defmacro table (&body body)
-  "first paramter can be t, nil, number or anything else"
-  `(let ((*table* (make-instance 'table))
-	 (*table-float-digits* ,(if (numberp (first body)) (first body) nil)))
-     ,@(append body
-	       (unless (eql (first body) nil)
-		 (list `(table-handle-floats (slot-value *table* 'list)))))))
-
-(defun table-data ()
-  (slot-value *table* 'list))
-
-(defun row (&rest values)
-  (with-slots (list posi posj) *table*
-    (setf list (append list (list values))
-	  posi (1+ posi)
-	  posj 0)))
-
-(defun plot-printer (data &optional (type :xy) (format-string "~&~,10f ~,10f"))
-  (if (typep type 'list)
-      (lambda ()
-	(loop for d in data
-	      with xcol = (first type)
-	      with ycol = (second type) do
-		(format t format-string (nth xcol d) (nth ycol d))))
-      (ecase type
-	(:xy (lambda ()
-	       (loop for (x y) in data do
-		 (format t format-string x y))))
-	(:xxyy (lambda ()
-		 (loop for x in (first data)
-		       for y in (second data) do
-			 (format t format-string x y))))
-	(:y (lambda ()
-	      (loop for y in data
-		    for i from 0 do
-		      (format t format-string i y)))))))
-
-(ql:quickload :eazy-gnuplot)
-(defparameter *ezy-file* nil)
-(defmacro with-plot (filename &body body)
-  `(let ((*ezy-file* (merge-pathnames ,filename *img-path*)))
-     (eazy-gnuplot:with-plots (*standard-output* :debug t)
-       ,@body)
-     (format nil "./img/~a" ,filename)))
-
-(defun setup (&rest args &key (xlabel "x") (ylabel "y")
-				(terminal "png")
-				(key '(:bottom :right :font "Times New Roman,20"))
-	      &allow-other-keys)
-  
-  (apply #'eazy-gnuplot:gp-setup :output *ezy-file*
-				 :xlabel xlabel :ylabel ylabel
-				 :terminal terminal
-				 :key key 
-				 (uiop:remove-plist-keys '(:xlabel :ylabel :terminal :key) args)))
-
-(defun ezplot (data &rest args &key (title "Plot") (using '(1 2)) (with '(:lines))
-	       &allow-other-keys)
-  (apply #'eazy-gnuplot:plot (plot-printer data (mapcar #'1- using))
-	 :using using
-	 :with with
-	 :title title
-	 (uiop:remove-plist-keys '(:using :with :title) args)))
-
-(defun plot-example () 
-  (eazy-gnuplot:with-plots (*standard-output* :debug nil)
-      (eazy-gnuplot:gp-setup :xlabel "x-label"      
-			     :ylabel "y-label"
-			     :output file 
-			     :terminal "png"
-			     :key '(:bottom :right :font "Times New Roman, 20")
-			     :pointsize "0.4px")
-      (eazy-gnuplot:plot (plot-printer data :xy)
-			 :using '(1 2)
-			 :title "title"
-			 :with '(:lines))))
-
-(defun plot (data file &key (type :xy) (gui nil) (multiple nil) title)
-  (let ((terminal (if gui :qt :png)))
-    (eazy-gnuplot:with-plots (*standard-output* :debug t)
-      (eazy-gnuplot:gp-setup :xlabel "x-label"      ; strings : "\"x-label\""
-			     :ylabel "y-label"
-			     :output file ; pathnames : "\"sample.png\""
-			     :terminal terminal         ; keyword/symbols: "terminal png"
-			     
-			     ;; list contents are recursively quoted, then joined by a space
-			     :key '(:bottom :right :font "Times New Roman, 20")
-			     
-			     :pointsize "0.4px"
-			     
-			     ;;:yrange :|[0:1]|
-			     ;; currently, specifying these kinds of options requires to abuse
-			     ;; keywords and symbols. Another example: comma separated list, e.g.,
-			     ;; :terminal '(:png :size |10cm,6cm|)
-			     ;;
-			     ;; 2/4/2016 Major options are now covered. 
-			     )
-
-      ;; any unsupported commands are available by printing it to the stream
-      ;;(format t "~%unset key")
-      
-      ;; We are extending its expressivity. For example, as of 39d60d, there is gp-unset and gp-set.
-      ;; An equivalent of above is (gp-unset :keys) .
-      ;; The list is growing!
-
-      ;; Functions can be protted with func-plot
-      ;;(plot "sin(x)" :title "super sin curve!")
-      ;; Plot a lisp data directly
-      (eazy-gnuplot:plot (plot-printer data type)
-			 :using '(1 2)
-			 :title (if multiple (first title) title)
-			 :with '(:lines))
-      (when multiple
-	(loop for i from 3 to (length (first data)) do 
-	  (eazy-gnuplot:plot (plot-printer data (list 0 (1- i)))
-			     :using '(1 2)
-			     :title (nth (- i 2) title)
-			     :with '(:lines))))
-      (if gui
-	  (format t "~&pause mouse button2;~%")))))
-
-(defparameter *img-path* (asdf:system-relative-pathname :moi "../workbook/img/"))
-(defun plot-table (&key file (type :xy) (gui nil) (title "line"))
-  (plot (slot-value *table* 'list) (merge-pathnames file *img-path*) :type type :gui gui :multiple (listp title) :title title)
-  (format nil "img/~a" file))
-
-(defun plot2 (data filename &optional (titles "line"))
-  (plot data (merge-pathnames filename *img-path*) :type :xy :gui nil :multiple (listp titles) :title titles)
-  (format nil "img/~a" filename))
 
 (ql:quickload :cl-mathstats)
 (defun linear-fit (&key (data (slot-value *table* 'list)))
@@ -238,7 +89,7 @@
   (mapcar #'(lambda (n)
 	      (cond ((numberp n) n)
 		    (t (parse-float:parse-float n :type 'double-float :exponent-character #\d))))
-	  (butlast (mapcar #'first (rest fit)) 2)))
+	  (butlast (mapcar #'first (rest fit)) 3)))
 
 
 (defun max-diff (fit &optional (data *bigdata*))
@@ -251,19 +102,38 @@
 	for fit-tp = (funcall fit x)
 	maximizing (abs (* 100 (/ (- tp fit-tp) fit-tp)))))
 
-(defun max-fit-%diff (func initial &optional (data *bigdata*))
-  (multiple-value-bind (params r^2) (fit::fit func data initial t nil)
+(defun save-fit-func (func params)
+  (lambda (x)
+    (apply func x params)))
+
+(defun max-fit-%diff (func initial &key (data *bigdata*) (save nil))
+  (multiple-value-bind (params r^2 rmse covariance) (fit::fit func data initial t nil)
+    (if save
+	(setf (symbol-value save) (save-fit-func func params)))
     (table 6
-      (row "max %diff" "R^2" "Params")
+      (row "max %diff" "R^2" "rmse" "Params" "Covariance")
       (row (loop for (x tp) in data
 		 for fit-tp = (apply func x params)
 		 maximizing (abs (* 100 (/ (- tp fit-tp) fit-tp))))
-	    r^2
-	    params))))
+	   r^2
+	   rmse
+	   params
+	   (loop for i from 0 below (length initial) collect (grid:aref covariance i i))))))
 
 (defun map-data (func &optional (data *bigdata*))
   (loop for (x tp) in data do
     (funcall func x tp)))
+
+(defun filter-data(&key n h bw bx by)
+  (loop for d in *bigdata*
+	for (x tp) = d
+	for (nn hh bwbw bxbx byby) = x
+	when (and (or (not n) (= n nn))
+		  (or (not h) (= h hh))
+		  (or (not bw) (= bw bwbw))
+		  (or (not bx) (= bx bxbx))
+		  (or (not by) (= by byby)))
+	  collect d))
 
 (defun fit-check-csv (fit-func x-func file &optional (data *bigdata*))
   (cl-csv:write-csv
@@ -280,3 +150,111 @@
 	 collect  (list tp fitted-tp))
    
    :stream (merge-pathnames file *img-path*)))
+
+(defun scatter-plot (fit-func file &optional (data *bigdata*))
+  (let ((tp-and-fit (loop for (x tp) in data 
+			  for fitted-tp = (funcall fit-func x)
+			  collect  (list tp fitted-tp))))
+    (with-plot file
+      (setup :xlabel "Exact T" :ylabel "Fitted T" 
+	     :terminal '(:png :size :|1500,1100| :font "Times New Roman, 30pt"))
+      (ezplot tp-and-fit :title "Fit" :with '(:point))
+      (eazy-gnuplot:plot "x" :with '(:lines) :title "Exact"))))
+
+(defun comparision-scatter-plot (fit-func other-funcs titles file &optional (data *bigdata*))
+  (let ((tp-and-fit (loop for (x tp) in data 
+			  for fitted-tp = (funcall fit-func x)
+			  collect  (list tp fitted-tp)))
+	(sorteddata (sort data #'< :key #'second)))
+    (with-plot file
+      (setup :xlabel "Exact T" :ylabel "Fitted T" 
+	     :terminal '(:png :size :|1500,1100| :font "Times New Roman, 30pt"))
+      (ezplot tp-and-fit :title "Fit" :with '(:point))
+      (eazy-gnuplot:plot "x" :with '(:lines) :title "Exact")
+      (loop for o in other-funcs
+	    for title in titles do
+	(ezplot (loop for (x tp) in sorteddata
+		      for fitted-tp = (funcall o x)
+		      collect (list tp fitted-tp))
+		:title title
+		:with :point)))))
+		
+
+(defun fit-plot (fit-func x-axis file &optional (data *bigdata*))
+  (let ((x-tp-fit (sort (loop 
+			  for (x tp) in data 
+			  collect (list (funcall x-axis x) tp (funcall fit-func x)))
+			#'< :key #'first)))
+    (with-plot file
+      (setup :xlabel "X-axis" :ylabel "Tp"
+	     :terminal '(:png :size :|1500,1100| :font "Times New Roman, 30pt"))
+      (ezplot x-tp-fit :title "Fit" :using '(1 3) :with '(:lines))
+      (ezplot x-tp-fit :title "Exact" :using '(1 2) :with '(:point)))))
+
+(defun /Nh (x)
+  (* (first x) (second x)))
+
+(defun sd1 (sn^2 sn n)
+  "Stanard deviation"
+  (sqrt (- (/ sn^2 n)
+	   (expt (/ sn n) 2))))
+
+(defun cv1 (sn^2 sn n)
+  "Coefficient of variance"
+  (/ (sd1 sn^2 sn n)
+     (/ sn n)))
+
+(defun sensitivity (param filtered-data)
+  (let ((parami (1- (ecase param
+		      (:n 1) (:h 2) (:bw 3) (:bx 4) (:by 5)))))
+    (loop for (x tp) in filtered-data
+	  for p = (nth parami x)
+	  summing tp into stp
+	  summing (expt tp 2) into  stp^2
+	  summing p into sp
+	  summing (expt p 2) into sp^2
+	  summing 1 into count 
+	  finally (return (/ (cv1 stp^2 stp count)
+			     (cv1 sp^2  sp  count))))))
+
+(defun remove-nth (n list)
+  (nconc (subseq list 0 n) (nthcdr (1+ n) list)))
+(defparameter *bigdata* nil)
+
+(defun max-effect (param &optional (data *bigdata*))
+  (let ((parami (1- (ecase param
+		      (:n 1) (:h 2) (:bw 3) (:bx 4) (:by 5) (:nh 6) (:bxby 7))))
+	(table (make-hash-table :test 'equal)))
+    (loop for (x tp) in data
+	  for p =  (if (> parami 4) nil (nth parami x))
+	  for others = (cond ((= parami 5) (nthcdr 2 x))
+			     ((= parami 6) (butlast (butlast x)))
+			     (t (remove-nth parami x)))
+	  for (min max) = (or (gethash others table) '(100 0)) do
+	    (setf (gethash others table) (list (min min tp) (max max tp))))
+    (loop for o being the hash-key of table
+	  for (min max) = (gethash o table)
+	  with maxdiff = 0
+	  with maxo = nil do
+	    (when (< maxdiff (- max min))
+	      (setf maxdiff (- max min)
+		    maxo o))
+	  finally (return (values maxdiff maxo)))))
+    
+
+(defun histogram (list &key (min (reduce #'min list)) (max (reduce #'max list)) (steps 1))
+  (let* ((size (ceiling (- max min) steps))
+	 (index (mapcar (lambda (n)
+			  (min (max  0 (floor (- n min) steps)) size))
+			list))
+	 (histogram (make-array size :element-type 'fixnum :initial-element 0)))
+    (loop for i in index do
+      (incf (aref histogram i)))
+    histogram))
+
+
+(defun histogram-list (list &key (min (reduce #'min list)) (max (reduce #'max list)) (steps 1))
+  (let ((hist (histogram list :min  min :max  max :steps steps)))
+    (loop for v across hist
+	  for x from min by steps
+	  collect (list x v))))
